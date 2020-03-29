2e5827815fd9a678ab8eb7791fb559e860920cb9
Junjie HU <jeremy.junjie.hu@gmail.com>
2020-03-29 20:06:49 +0200

    fix scraping and word cloud plot (clear pic)

--- DEDA_class2019_SYSU_Abstract_LDA_wordcloud/DEDA_class2019_SYSU_Abstract_LDA_wordcloud.py
+++ DEDA_class2019_SYSU_Abstract_LDA_wordcloud/DEDA_class2019_SYSU_Abstract_LDA_wordcloud.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import matplotlib.pyplot as plt
 import re
 from nltk.corpus import stopwords
@@ -10,55 +8,46 @@ from wordcloud import WordCloud, STOPWORDS
 import numpy as np
 
 # Please change the working directory to your path!
-#os.chdir("/Users/xinwenni/LDA-DTM/xmas_song") 
-d = os.getcwd()
+# os.chdir("/Users/xinwenni/LDA-DTM/xmas_song")
+sub_dir = os.getcwd() + '/DEDA_class2019_SYSU_Abstract_LDA_wordcloud/'
+cwd_dir = sub_dir if os.path.exists(sub_dir) else os.getcwd()  # the path you save your files
 
-raw_text= open('Abstract_all.txt','r',encoding='utf-8').read()
+raw_text = open(cwd_dir + 'Abstract_all.txt', 'r', encoding='utf-8').read()
 raw_text = str(raw_text)
-raw_text=  re.sub('\n',' ',raw_text)
+raw_text = re.sub('\n', ' ', raw_text)
 
 cleantextprep = str(raw_text)
 
-
 # keep only letters, numbers and whitespace
-expression = "[^a-zA-Z0-9 ]" 
-cleantextCAP = re.sub(expression, '', cleantextprep) # apply regex
-cleantext = cleantextCAP.lower() # lower case 
+expression = "[^a-zA-Z0-9 ]"
+cleantextCAP = re.sub(expression, '', cleantextprep)  # apply regex
+cleantext = cleantextCAP.lower()  # lower case
 
-
-
-text_file = open("Output_total.txt", "w")
-text_file.write(str(cleantext))
-text_file.close()
-
-
-root_path = os.getcwd()
+with open(cwd_dir + "Output_total.txt", "w")as text_file:
+    text_file.write(str(cleantext))
 
 # Read the whole text.
-with open(path.join(root_path, 'Output_total.txt'), 'r', encoding='utf-8', errors='ignore') as outout_file:
+with open(path.join(cwd_dir, 'Output_total.txt'), 'r', encoding='utf-8', errors='ignore') as outout_file:
     text = outout_file.readlines()
 
 # Mask
-#xmas_tree_pic = np.array(Image.open(path.join(root_path, "xmas_tree2.png")))
-ql_pic = np.array(Image.open('qletlogo_tr.png'))
+# xmas_tree_pic = np.array(Image.open(path.join(cwd_dir, "xmas_tree2.png")))
+ql_pic = np.array(Image.open(cwd_dir + 'QuantletsLogo_Ring.jpg'))
 
 # Optional additional stopwords
 stopword = set(STOPWORDS)
-stopword = stopword.union({'abstract','keywords','sep'})
+stopword = stopword.union({'abstract', 'keywords', 'sep'})
+
 # Construct Word Cloud
 # no backgroundcolor and mode = 'RGBA' create transparency
 wc = WordCloud(max_words=100, stopwords=stopword, mask=ql_pic, mode='RGBA', background_color=None)
-
 
 # Pass Text
 wc.generate(text[0])
 
 # store to file
-wc.to_file(path.join(root_path, "wordcloud_abstract.png"))
-
-
-
+plt.figure(figsize=(10,10))
 plt.imshow(wc, interpolation='bilinear')
 plt.axis("off")
 plt.show()
-
+plt.savefig(cwd_dir + "wordcloud_abstract.png", dpi=300, transparent=True)
